d47a6e40756e2f1c2d0c08eacadc922249df49e5
JeshuranJebanesan <jeshuranj@outlook.com>
2022-11-23 00:12:05 +0000

    Question 3 completed successfully

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(q2());
+        Console.WriteLine(q3());
     }
 
     //Answer is 233168
@@ -34,8 +34,8 @@ class Program
         //Use arithmetic sum formula a (a(3) + a(5) - a(15)) with bigO(1)
     }
 
-    //Answer is 4613732
-    static int q2 () {
+    //Answer is 461373
+    static int q2() {
         //Method 1 
         //Uses basic brute force method
         //Instead of checking if term is even, could have a counter capped at 3 as even terms occur every 3 terms
@@ -83,5 +83,44 @@ class Program
         }
 
         return total;
+    }
+
+    //Answer is 6857
+    static long q3() {
+        //Method 1
+        //Uses basic brute force
+        //This brute force has a helper function to determine if a number is prime adding the number if so to a list of primes
+        //The complete list of primes will be calculated by finding the factors after the sqrt of the number and checking whether they too are prime
+        bool isPrime(long n) {
+            List<long> primes = new List<long>();
+            bool isPrime;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++ ) {
+                isPrime = true;
+                foreach (int prime in primes) {
+                    if (i % prime == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                primes.Add(i);
+            }
+
+            isPrime = true;
+            foreach (int prime in primes) {
+                if (n % prime == 0) { return false; }
+            }
+            return isPrime;
+        }
+        List<long> primes = new List<long>();
+
+        for (long i = 2; i <= (600851475143); i++ ) {
+            if (isPrime(i) && (600851475143 % i == 0)) {
+                primes.Add(i);
+                Console.WriteLine(i);
+            }
+        } 
+
+        return primes.Last();
     }
 }
